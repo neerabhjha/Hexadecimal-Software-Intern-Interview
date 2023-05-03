7b28d7f55adec02c483f0bca180ea4bbcbc26f4e
neerabhjha <jhaneerabh@gmail.com>
2023-05-03 11:06:45 +0530

    .env added

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
+require("dotenv").config("./.env");
 
 module.exports = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://NeerDB:L78UlXlTFLutVPHf@neercluster.fyi9egt.mongodb.net/practiceDB",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to DB!");
   } catch (error) {
     console.log(error);
